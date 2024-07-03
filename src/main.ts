3b4f4af06ec71426a2997fa768b6832b9529440a
Khun <khun.ly@bstorm.be>
2024-07-03 15:41:26 +0200

    add open API

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // activer le partage des ressourses inter origine
+  // activer le partage des ressources inter origine
   app.enableCors();
 
   // ajouter un middleware de validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // configuration OPEN API
+  const config = new DocumentBuilder()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
   await app.listen(3000, () => 
     console.log('listening to port 3000')
